9dcbbbe4cb9e7ba90c74f62963f8a005cf68cfdf
Olivier DEPREZ <olivier.deprez@gmail.com>
2023-06-02 11:36:42 +0200

    chore(db): launching synchronous db to see errors

--- db/run_db.sh
+++ db/run_db.sh
@@ -4,5 +4,6 @@ PWD=$(pwd)
 
 echo "docker run --name postgresql -e POSTGRES_USER=wcsbot_dbuser -e POSTGRES_PASSWORD=xxx -p 5432:5432 -v ${PWD}:/var/lib/postgresql/data -d postgres"
 
-docker run --name postgresql -e POSTGRES_USER=wcsbot_dbuser -e POSTGRES_PASSWORD=wcsbot_dbpass -p 5432:5432 -v ${PWD}:/var/lib/postgresql/data -d postgres
+#docker run --name postgresql -e POSTGRES_USER=wcsbot_dbuser -e POSTGRES_PASSWORD=wcsbot_dbpass -p 5432:5432 -v ${PWD}:/var/lib/postgresql/data  postgres
 
+docker run --name postgresql -e POSTGRES_USER=wcsbot_dbuser -e POSTGRES_PASSWORD=wcsbot_dbpass -p 5432:5432   postgres
